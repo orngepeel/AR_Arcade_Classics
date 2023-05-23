f7ce705ec33ae3ae77e73d767bb06de21c10dfbf
Devon Braner <68525598+orngepeel@users.noreply.github.com>
2023-05-23 01:08:58 -0400

    trying destroy with explicit gameObject

--- Assets/Scripts/Runtime/UI/StartButton.cs
+++ Assets/Scripts/Runtime/UI/StartButton.cs
@@ -5,19 +5,21 @@ namespace UnityEngine.XR.ARFoundation.ARcadeClassics
     public class StartButton : MonoBehaviour
     {
 
-[SerializeField] GameObject snakeHeadPrefab;
-[SerializeField] GameObject gameBoard;
-[SerializeField] GameObject GameStartPrefab;
+        [SerializeField] GameObject snakeHeadPrefab;
+        [SerializeField] GameObject gameBoard;
+        [SerializeField] GameObject GameStartPrefab;
 
-public void StartButtonPressed()
-{
-      Destroy(GameStartPrefab);
-  Vector3 localPosition = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));; // some logic for getting the local position relative to the board
-  Quaternion localRotation = Quaternion.identity; // some logic for getting local rotation relative to the board
-  GameObject SnakeHead = Instantiate(snakeHeadPrefab, localPosition, localRotation, gameBoard.transform);
-  // whatever else needs to happen when the game starts
-  
-}
+        public void StartButtonPressed()
+        {
+        
+        Vector3 localPosition = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f, 0.5f)); // some logic for getting the local position relative to the board
+        Quaternion localRotation = Quaternion.identity; // some logic for getting local rotation relative to the board
+        GameObject SnakeHead = Instantiate(snakeHeadPrefab, localPosition, localRotation, gameBoard.transform);
+        // whatever else needs to happen when the game starts
+        
+        Destroy(GameStartPrefab.gameObject);
+
+        }
     }
 }
 
