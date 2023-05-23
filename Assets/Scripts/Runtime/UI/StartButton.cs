9d2413639c60a7900054d7e0b40fa236daad7779
Benni Taylor <85967594+bunnybenni@users.noreply.github.com>
2023-05-23 00:47:46 -0400

    button + snake prefabs
    got the start button to render on the screen instead of the capsule, added prefabs for the snake + snake body + gameboard, added logic for the snake moving forward

--- Assets/Scripts/Runtime/UI/StartButton.cs
+++ Assets/Scripts/Runtime/UI/StartButton.cs
@@ -4,34 +4,21 @@ namespace UnityEngine.XR.ARFoundation.ARcadeClassics
 {
     public class StartButton : MonoBehaviour
     {
-        [SerializeField]
-        GameObject m_StartButton;
-        [SerializeField]
-        private string GameName;
-        [SerializeField]
-        GameObject GameStartPrefab;
-        GameObject ActivatedGame;
 
-        public GameObject backButton
-        {
-            get => m_StartButton;
-            set => m_StartButton = value;
-        }
+[SerializeField] GameObject snakeHeadPrefab;
+[SerializeField] GameObject gameBoard;
+[SerializeField] GameObject GameStartPrefab;
 
-        void Start()
-        {
-            if (Application.CanStreamedLevelBeLoaded(GameName))
-                m_StartButton.SetActive(true);
-        }
-
-        public void StartButtonPressed()
-        {
-            if (Application.CanStreamedLevelBeLoaded(GameName))
-                ActivatedGame = GameObject.Find("/"+GameName);
-                ActivatedGame.SetActive(true);
-                ActivatedGame.transform.parent = GameStartPrefab.transform.parent;
-                GameStartPrefab.SetActive(false);
-                
-        }
+public void StartButtonPressed()
+{
+      Destroy(GameStartPrefab);
+  Vector3 localPosition = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));; // some logic for getting the local position relative to the board
+  Quaternion localRotation = Quaternion.identity; // some logic for getting local rotation relative to the board
+  GameObject SnakeHead = Instantiate(snakeHeadPrefab, localPosition, localRotation, gameBoard.transform);
+  // whatever else needs to happen when the game starts
+  
+}
     }
 }
+
+
