c2e6c968be6314ddaddeb7da7c90b7f194066e8c
Devon Braner <68525598+orngepeel@users.noreply.github.com>
2023-05-26 14:38:13 -0400

    this whole time it was the wrong script!

--- Assets/Scripts/Runtime/SavePosition.cs
+++ Assets/Scripts/Runtime/SavePosition.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class SavePosition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public ARTrackedImageManager trackedImageManager;
+
+    private string anchorKey = "AnchorKey";
+
+    private void OnEnable()
     {
-        
+        trackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
+        trackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (var trackedImage in eventArgs.updated)
+        {
+            SaveAnchor(trackedImage);
+        }
+    }
+
+    private void SaveAnchor(ARTrackedImage trackedImage)
+    {
+        string positionKey = anchorKey + "Position";
+        string rotationKey = anchorKey + "Rotation";
         
+        PlayerPrefs.SetString(positionKey, Vector3ToString(trackedImage.transform.position));
+        PlayerPrefs.SetString(rotationKey, QuaternionToString(trackedImage.transform.rotation));
+    }
+
+    private string Vector3ToString(Vector3 vector)
+    {
+        return vector.x + "," + vector.y + "," + vector.z;
+    }
+
+    private string QuaternionToString(Quaternion quaternion)
+    {
+        return quaternion.x + "," + quaternion.y + "," + quaternion.z + "," + quaternion.w;
     }
 }
+
