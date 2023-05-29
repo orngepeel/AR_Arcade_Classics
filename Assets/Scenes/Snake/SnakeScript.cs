3eaf115a29442ecb6b2e81c3134352ac051d9709
Benni Taylor <85967594+bunnybenni@users.noreply.github.com>
2023-05-29 19:54:56 -0400

    snake head added, moves indefinitely up the z axis

--- Assets/Scenes/Snake/SnakeScript.cs
+++ Assets/Scenes/Snake/SnakeScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SnakeScript : MonoBehaviour
 {
-    public float snakeSpeed = 5;
+    public float snakeSpeed = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +14,37 @@ public class SnakeScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.position * snakeSpeed * Time.deltaTime;
+        transform.position = transform.position + (transform.forward * snakeSpeed * Time.deltaTime);
+
+      //  float upInput = Input.GetAxis("Up");
+       // float downInput = Input.GetAxis("Down");
+      //  float leftInput = Input.GetAxis("Left");
+       // float rightInput = Input.GetAxis("Right");
+
+        //if key == "up":
+        // y == on, positive
+        // x == off
+       // transform.position = transform.position + new Vector3(0, 0, verticalInput * snakeSpeed * Time.deltaTime);
+
+        //if key == "down":
+        // y == on, negative
+        // x == off
+       // transform.position = transform.position + new Vector3(0, 0, verticalInput * -1 * snakeSpeed * Time.deltaTime);
+
+        //if key == "left":
+        // y == off
+        // x == on, negative
+        //transform.position = transform.position + new Vector3(horizontalInput * -1 * snakeSpeed * Time.deltaTime, 0, 0);
+
+        //if key == "right":
+        // y == off
+        // x == on, positive
+        //transform.position = transform.position + new Vector3(horizontalInput * snakeSpeed * Time.deltaTime, 0, 0);
+
+        //update the position
+        //transform.position = transform.position + new Vector3(horizontalInput * snakeSpeed * Time.deltaTime, 0, verticalInput * snakeSpeed * Time.deltaTime);
+
+        //output to log the position change
+     //   Debug.Log(transform.position);
     }
 }
