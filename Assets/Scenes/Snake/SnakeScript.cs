76a947f67bc2332999db5174e0aa135327567770
Benni Taylor <85967594+bunnybenni@users.noreply.github.com>
2023-05-29 22:18:25 -0400

    added "food" prefab; got collision set up/working

--- Assets/Scenes/Snake/SnakeScript.cs
+++ Assets/Scenes/Snake/SnakeScript.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class SnakeScript : MonoBehaviour
 {
+    public float steerSpeed = 180;
     public float snakeSpeed = 1;
+    public Vector3 startPosition;
     // Start is called before the first frame update
     void Start()
     {
+        startPosition = transform.position;
         
     }
 
@@ -16,35 +19,26 @@ public class SnakeScript : MonoBehaviour
     {
         transform.position = transform.position + (transform.forward * snakeSpeed * Time.deltaTime);
 
-      //  float upInput = Input.GetAxis("Up");
-       // float downInput = Input.GetAxis("Down");
-      //  float leftInput = Input.GetAxis("Left");
-       // float rightInput = Input.GetAxis("Right");
 
-        //if key == "up":
-        // y == on, positive
-        // x == off
-       // transform.position = transform.position + new Vector3(0, 0, verticalInput * snakeSpeed * Time.deltaTime);
+    }
 
-        //if key == "down":
-        // y == on, negative
-        // x == off
-       // transform.position = transform.position + new Vector3(0, 0, verticalInput * -1 * snakeSpeed * Time.deltaTime);
+    void Grow() 
+    {
 
-        //if key == "left":
-        // y == off
-        // x == on, negative
-        //transform.position = transform.position + new Vector3(horizontalInput * -1 * snakeSpeed * Time.deltaTime, 0, 0);
+    }
 
-        //if key == "right":
-        // y == off
-        // x == on, positive
-        //transform.position = transform.position + new Vector3(horizontalInput * snakeSpeed * Time.deltaTime, 0, 0);
+    void ResetState()
+    {
+        transform.position = startPosition;
 
-        //update the position
-        //transform.position = transform.position + new Vector3(horizontalInput * snakeSpeed * Time.deltaTime, 0, verticalInput * snakeSpeed * Time.deltaTime);
+    }
 
-        //output to log the position change
-     //   Debug.Log(transform.position);
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Food")) {
+            Grow();
+        } else if (other.gameObject.CompareTag("Obstacle")) {
+            ResetState();
+        }
     }
 }
