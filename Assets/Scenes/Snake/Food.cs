fd32fa6f9e23e61b27356c11ff1c3807a3b8ba0f
Benni Taylor <85967594+bunnybenni@users.noreply.github.com>
2023-05-30 22:01:04 -0400

    fixed collider size of food; randomized position and movement still not working

--- Assets/Scenes/Snake/Food.cs
+++ Assets/Scenes/Snake/Food.cs
@@ -9,13 +9,17 @@ public class Food : MonoBehaviour
     public BoxCollider GridArea;
 
     private void RandomizePosition() {
-        Bounds bounds = this.GridArea.bounds;
+        Bounds bounds = GridArea.bounds;
 
         float x = Random.Range(bounds.min.x, bounds.max.x);
         float y = Random.Range(bounds.min.y, bounds.max.y);
         float z = Random.Range(bounds.min.z, bounds.max.z);
 
-        transform.position = new Vector3(x, startPosition.y, z);
+        transform.position = new Vector3(x, y, z);
+                //Output to the console the center and size of the Collider volume
+
+        Debug.Log("Collider bound Minimum : " + bounds.min);
+        Debug.Log("Collider bound Maximum : " + bounds.max);
     }
 
     // Start is called before the first frame update
